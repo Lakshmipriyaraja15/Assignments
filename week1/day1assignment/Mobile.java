68582e05b9a77d5c0f26e99fb0d6254fe5b21451
Lakshmipriyaraja15 <124476755+Lakshmipriyaraja15@users.noreply.github.com>
2023-02-05 07:25:21 +0530

    Add files via upload

---
+++ week1/day1assignment/Mobile.java
@@ -0,0 +1,23 @@
+package week1.day1assignment;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileBrandName="Mi";
+		System.out.println(mobileBrandName);
+		char mobileLogo='$';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=20;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=12345;
+		System.out.println(modelNumber);
+		long mobileImeiNumber=112323223;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice=12345.234f;
+		System.out.println(mobilePrice);
+		boolean isDamaged=true;
+		System.out.println("isDamaged"+isDamaged);
+
+	}
+
+}
---
+++ week1/day1assignment/TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day1assignment;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		int noOfWheels=2;
+		System.out.println(noOfWheels);
+		short noOfMirrors=2;
+		System.out.println(noOfMirrors);
+		long chassisNumber=213123456789l;
+		System.out.println(chassisNumber);
+		boolean isPunctured=false;
+		System.out.println(isPunctured);
+		String bikeName="Honda";
+		System.out.println(bikeName);
+		double runningKm=1230.123;
+		System.out.println(runningKm);
+
+	}
+
+}
